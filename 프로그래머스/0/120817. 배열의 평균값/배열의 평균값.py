fb19d4a29d66a075640a5fd5e15217d7ffcc1d28
narth8252 <narth8252@gmail.com>
2025-06-05 20:56:32 +0900

    [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,36 @@
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    return answer
+
+# import numpy as np
+# def solution(numbers):
+#     return np.mean(numbers)
+
+# def solution(numbers):
+#     return sum(numbers) / len(numbers)
+
+
+# def solution(numbers):
+#     answer = 0
+#     count = len(numbers)
+#     for i in range(count):
+#         answer += numbers[i]
+#     return answer/count
+
+# def solution(numbers):
+#     answer = 0
+#     for i in numbers:
+#         answer += i
+#     return answer / len(numbers)
+
+
+# def solution(numbers):
+#     answer = 0
+#     return sum(numbers)/len(numbers)
+
+# def solution(numbers):
+#     answer = 0
+#     for i in numbers:
+#         answer += i
+#     answer /= len(numbers)
+#     return answer
