c1e23baaf7492c7d334575b868080ef9269463ae
narth8252 <narth8252@gmail.com>
2025-06-07 23:15:47 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -6,3 +6,20 @@ def solution(my_string):
         # if char.lower() not in vowels: #대소문자 구분없이 검사
             result += char # 결과에 추가
     return result
+
+#다른풀이
+def solution(my_string):
+    return "".join([i for i in my_string if not(i in "aeiou")])
+
+def solution(my_string):
+    vowels = ['a','e','i','o','u']
+    for vowel in vowels:
+        my_string = my_string.replace(vowel, '')
+    return my_string
+
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i not in ['a','e','i','o','u']:
+            answer += i
+    return answer
