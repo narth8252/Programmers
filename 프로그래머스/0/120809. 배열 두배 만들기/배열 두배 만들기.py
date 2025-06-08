d06ebd9dd5f5cf997f96f6f2ec98109e781dc499
narth8252 <narth8252@gmail.com>
2025-06-08 21:29:14 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.18 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,11 +1,11 @@
 def solution(numbers):
-    return [2 * num for num in numbers]
+    return [num*2 for num in numbers]
             #새 리스트로[각원소를2배, for in 배열 순회]반환
 
-# def solution(numbers):
-#     return [2*int(digit) for digit in str(numbers)]
-# print(solution(123))  # [2, 4, 6]
-# print(solution(405))  # [8, 0, 10]
+def solution(numbers):
+    return [2*int(digit) for digit in str(numbers)]
+print(solution(123))  # [2, 4, 6]
+print(solution(405))  # [8, 0, 10]
 #파이썬에선 작동. 프로그래머스에서 오류
 #ValueError: invalid literal for int() with base 10: '['
 #int('[')처럼 숫자가 아닌 문자열을 정수로 바꾸려 했다는 뜻
@@ -17,6 +17,16 @@ def solution(numbers):
 """
 
 #다른풀이
+def solution(numbers):
+    answer = []
 
+    for i in numbers:
+        answer.append(i*2)
+    return answer
 
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        answer.append(numbers[i]*2)
+    return answer
 
