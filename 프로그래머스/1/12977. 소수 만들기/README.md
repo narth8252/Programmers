0499b18ecf81ad3683c89de1759159857a5fbe32
narth8252 <narth8252@gmail.com>
2025-06-10 13:41:18 +0900

    [level 1] Title: 소수 만들기, Time: 204.05 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/README.md
+++ 프로그래머스/1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 21.26 ms
+메모리: 9.13 MB, 시간: 204.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 10일 13:21:12
+2025년 06월 10일 13:41:13
 
 ### 문제 설명
 
--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,11 +1,55 @@
-from itertools import combinations
+# 코딩테스트 연습> 코딩테스트 입문> 소수만들기
+"""
+주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 
+숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, 
+nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 
+return 하도록 solution 함수를 완성해주세요.
+
+ 제한사항
+nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+
+ 입출력 예
+nums	    result
+[1,2,3,4]	1
+[1,2,7,6,4]	4
+[1,2,4]를 이용해서 7을 만들 수 있습니다.
+"""
+#1.3개 숫자조합구하기
+# from itertools import combinations
+
+# nums = [1, 2, 3, 4, 5]
+# for comb in combinations(nums, 3):
+#     print(comb)
+
+#2.소수판별
+#소수:1과 자기자신만 나눠떨어지는 2이상의 양의정수
+#7을 나누었을 때 나누어 떨어지는 수는 1과 7뿐. 2~6으로 나누면 안떨어짐.
+#계산: 2부터 n의 제곱근까지만 검사하여 나눠떨어지는 수가 있는지?
+# import math
+# def is_prime(n):
+#     if n < 2:
+#         return False #2이상의 수만 소수
+#     for i in range(2, int(math.sqrt(n)) + 1):
+#         if n%i == 0:
+#             return False #나눠떨어지는 수는 소수아님
+#     return True #나누어떨어지는 수없으면 소수
+
+#3.조합 합이 소수인지 확인후 개수세기
+#조합을 하나씩 꺼내 합구한뒤, 소수인지 판단
+#소수라면 결과 개수를 1씩 증가
+
+#최종 : 소수 판별 효율을 위해 제곱근까지만 검사
+nums = [1,2,7,6,4]
+from itertools import combinations #as cb:이후 코드에서 combinations() 대신 cb()를 사용하실 수 있습니다.
 import math
 
 def is_prime(num):
     if num < 2:
         return False
     for i in range(2, int(math.sqrt(num))+1):
-        if num%i == 0:
+                  #(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
@@ -16,10 +60,21 @@ def solution(nums):
         if is_prime(total):
             cnt += 1
     return cnt
-    
 
-"""
-조합 사용하기: 3개의 서로 다른 숫자를 고를 때, 파이썬 itertools 모듈의 combinations 함수를 활용하면 편리합니다.
-소수 판별하기: 어떤 수가 소수인지 판별할 때에는 2부터 그 수의 제곱근까지 나누어떨어지는지 확인하는 방법이 효과적입니다.
-합 계산: 3개의 숫자 조합 각 합을 구해서 소수인지 확인하면 됩니다.
-"""
+#다른풀이1.
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for i in cb(nums, 3):
+        prime_sum = sum(i)
+        for j in range(2, prime_sum):
+            if prime_sum%j==0:
+                break
+        else:
+            answer += 1
+    return answer
+
+
+#  itertools 모듈은 반복(iteration) 작업을 도와주는 여러 가지 효율적인 함수를 포함
+# combinations는 리스트나 튜플같은 자료형에서 순서에 무관하게 특정개수만큼 뽑는 모둔 경우의 수를 만들어주는 함수
+#[1,2,3]에서 2개씩 뽑는 조합을 모두 구하면 (1,2),(1,3),(2,3)
