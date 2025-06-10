3d306a7a1972e592cb5a385058eb5799c0e93b29
narth8252 <narth8252@gmail.com>
2025-06-10 10:38:06 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -12,3 +12,15 @@ def solution(num_list):
     n2 = int(''.join(result2)) if result2 else 0  # 짝수로 만든 수
     
     return n1 + n2  # 두 수의 합 반환
+
+#다른풀이
+def solution(num_list):
+    answer = 0
+    even = "" #홀수
+    odd = "" #짝수
+    for n in num_list:
+        if n%2!=0: #홀수면
+            even+=str(n) #홀수변수even안에 문자열로 넣어라
+        else:
+            odd+=str(n)
+    return int(even) + int(odd) #정수로바꿔 더해라
