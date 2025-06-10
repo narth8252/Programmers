42ed4581bb540c28556e836ffaa1a54631ee1b12
narth8252 <narth8252@gmail.com>
2025-06-10 15:01:50 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/README.md
+++ 프로그래머스/0/120854. 배열 원소의 길이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 원소의 길이 - 120854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 0.00 ms
+메모리: 9.11 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 10일 14:33:29
+2025년 06월 10일 15:01:45
 
 ### 문제 설명
 
--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -5,3 +5,16 @@ def solution(strlist):
         result.append(len(i))
     return result
     
+#다른풀이
+def solution(strlist):
+    answer = list(map(len, strlist))
+                #map(func)
+    return answer
+
+#리스트 컴프리헨션
+def solution(strlist):
+    return [len(str) for str in strlist]
+            #                   strlist의 각요소를 순차적으로 꺼내
+            #           str변수에 저장
+            #len(str)으로 문자열길이 구함
+            #그값을 새리스트[]에 차례로 넣기
