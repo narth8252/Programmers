a7f05e8f14c2e869753f760726c82f33d19c8bfb
narth8252 <narth8252@gmail.com>
2025-06-10 10:07:54 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -0,0 +1,12 @@
+def solution(n, numlist):
+    return [x for x in numlist if x%n==0]
+           #numlist안x를 하나씩보며 n배수인지?
+                              #x/n나머지0이면,
+
+#다른풀이
+def solution(n, numlist): #numlist안 숫자하나씩 차례로꺼내 num변수에 넣어보자
+    result = []
+    for num in numlist:
+        if num%n==0: #num이 n의배수인지 = num/n나머지0이면
+            result.append(num) #배수이면 result리스트에 append
+    return result
