a002de68b605d77bffc69c1b7b45b145cbeb3e6f
narth8252 <narth8252@gmail.com>
2025-06-12 10:44:25 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.py
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.py
@@ -1,3 +1,9 @@
 def solution(num_list, n):
     answer = num_list[n-1:]
     return answer
+
+# def solution(num_list, n):
+#     answer = []
+#     for i in range(n-1, len(num_list)):
+#         answer.appendd(num_list[i])
+#     return answer
