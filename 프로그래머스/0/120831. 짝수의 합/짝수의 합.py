bdf96537d4e1385e269146feb07c0e7b18b335be
narth8252 <narth8252@gmail.com>
2025-06-16 16:43:13 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -4,12 +4,17 @@ def solution(n):
     # i=0
     # s=0
     answer = 0
-    i = 2
+    i = 2 #i를 2로 시작 첫짝수
     while i<=n:
         answer+=i
-        i+=2
+        i += 2 #짝수만 더하려고 2씩증가
     return answer
-#     answer=0
-#     for i in range(2, n+1, 2):
-#         answer+= i
-#     return answer
+# 반복 순서: i = 2, 4, 6, 8, 10
+# 누적 결과: 0 → 2 → 6 → 12 → 20 → 30
+
+def solution(n):
+    answer = 0
+    i = 2 #2부터 시작, 첫번째 짝수
+    for i in range(2, n+1, 2): #2부터시작, n까지n+1, 2씩증가(짝수반복)
+        answer += i
+    return answer
