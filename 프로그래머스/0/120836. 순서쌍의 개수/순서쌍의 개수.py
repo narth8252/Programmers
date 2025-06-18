264d4ad3be36a0313c443eb30cde399f0b29f868
narTH <narth8252@gmail.com>
2025-06-18 10:47:53 +0900

    [level 0] Title: 순서쌍의 개수, Time: 68.79 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,4 +1,13 @@
 def solution(n):
+    answer = 0
+    for a in range(n):
+        if n % (a+1) == 0:
+            answer +=1
+    return answer
+
+
+
+def solution(n):
     cnt = 0
     for a in range(1, n+1):
         if n%a == 0:
